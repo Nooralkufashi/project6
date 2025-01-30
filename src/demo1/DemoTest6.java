93a0fb4cb55e7b39b6308a73eb18ea8022e605c2
hayde <hayde@Hayder.attlocal.net>
2025-01-30 14:49:11 -0600

    Divide logic

--- src/demo1/DemoTest6.java
+++ src/demo1/DemoTest6.java
@@ -3,6 +3,11 @@ package demo1;
 public class DemoTest6 {
 	public static void main(String args []) {
 		System.out.println("Java");
+		int a = 80;
+		int b = 10;
+		int result = a/b;
+		
+		System.out.println(result);
 		
 	}
 
